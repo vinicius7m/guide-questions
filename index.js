540383ea6eb713b657c575bc694c569474baf864
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-08-25 15:59:53 -0300

    aprendendo sobre as features do EJS, e manipulando arquivos estáticos com Express

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Estou dizendo para o Express usar o EJS como View Engine
 app.set('view engine', 'ejs');
+// Usando arquivos estáticos (.html, .css, .img), pasta padrão de mercado é "public"
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render("home");
-});
+app.get('/:nome/:lang', (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
 
-app.get('/profile', (req, res) => {
-    res.render("principal/perfil");
+    var exibirMsg = false;
+
+    var produtos = [
+        { nome: "Doritos", preco: 3.14 },
+        { nome: "Coca-Cola", preco: 5 },
+        { nome: "Leite", preco: 1.45 },
+        { nome: "Carne", preco: 15.00 },
+        { nome: "Red Bull", preco: 6.00 },
+        { nome: "Nescau", preco: 4.00 }
+    ];
+
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do Programador",
+        inscritos: 100,
+        msg: exibirMsg,
+        produtos: produtos,
+    });
 });
 
 app.listen(port, () => {
