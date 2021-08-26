0e39a1ebf5260dfff7c441b513b2a38443220893
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-08-26 14:24:22 -0300

    Concluindo estilo do formulário

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render("index");
+    res.render('index');
+});
+
+app.get('/perguntar', (req, res) => {
+    res.render('perguntar');
 });
 
 app.listen(port, () => {
