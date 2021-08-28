eafa1effaef7e00cc41db5b38bcfec44391f2e4c
Vinícius Moreira <vinicius.vsm777@gmail.com>
2021-08-28 10:13:41 -0300

    pegando os valores do formulário com body-parser

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
 
 // Estou dizendo para o Express usar o EJS como View Engine
 app.set('view engine', 'ejs');
 // Usando arquivos estáticos (.html, .css, .img), pasta padrão de mercado é "public"
 app.use(express.static('public'));
 
+/*
+    O body parser é o agente com o objetivo de traduzir os dados enviados pelo formulário
+    em uma estrutura JavaScript e aplicarmos em nosso Back-End
+*/
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Rotas
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -16,7 +25,15 @@ app.get('/perguntar', (req, res) => {
 });
 
 app.post('/salvar-pergunta', (req, res) => {
-    res.send('Formulário recebido!');
+
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    res.json({
+        msg: "Dados enviados com sucesso!",
+        titulo,
+        descricao
+    });
 });
 
 app.listen(port, () => {
